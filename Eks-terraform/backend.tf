10460c6fb3278387fbfc2b2ee526fec1ac038094
adetunji adeleke <133719401+aadetunji12@users.noreply.github.com>
2023-10-29 09:51:02 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "kube122" # Replace with your actual S3 bucket name
+    bucket = "kube222" # Replace with your actual S3 bucket name
     key    = "ubuntu/terraform.tfstate"
     region = "us-west-1"
   }
